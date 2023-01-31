495518bac2eb9dec0e39a2f08677e6bc77774d40
Kawsar Jamil <68264633+itskawsarjamil@users.noreply.github.com>
2023-01-31 12:02:27 +0600

    mongodb attached

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("foodie server is running");
 })
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.x8fgjzf.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    
+});
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`server is running on port : ${port}`);
